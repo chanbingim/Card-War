bbcf8fd14e1afc69e6989a4232cf3fcbd19f795b
chanbingim <127704542+chanbingim@users.noreply.github.com>
2026-08-04 01:40:30 +0900

    2026-08-04
    1. HistoryUI 구성끝

--- My project/Assets/Scripts/UI/Entity/TurnHistoryUI.cs
+++ My project/Assets/Scripts/UI/Entity/TurnHistoryUI.cs
@@ -6,18 +6,22 @@ using UnityEngine.UI;
 
 public class TurnHistoryUI : UIBase
 {
-    [SerializeField] private Image  _BackGround = null;
-    [SerializeField] private int    _ViewCount = 5;
+    [SerializeField] private Image          _BackGround = null;
+    [SerializeField] private List<UIBase>   _HistoryView;
+    [SerializeField] private int            _ViewCount = 5;
 
-    private List<GameObject>        _HistoryView;
+    private Queue<CardAction>       _act = null;
+  
+
+    private int                     _maxPage = 0;
+    private int                     _CurPage = 1;
+
     private int                     _Percent = 5;
 
-    void Awake()
+    void Start()
     {
-
-
-
-
+        _ViewCount = _HistoryView.Count;
+     
     }
 
     public void SettingHistoryData(int ViewPlayerIndex)
@@ -28,24 +32,35 @@ public class TurnHistoryUI : UIBase
             if (BattleMgr == null)
                 throw new ArgumentException("[TrunHistoryUI] Battle Manager NULL");
 
-            var act = BattleMgr.GetPlayerHistoryAction(ViewPlayerIndex);
-            if(act == null)
+            _act = BattleMgr.GetPlayerHistoryAction(ViewPlayerIndex);
+            if(_act == null)
                 throw new ArgumentException("[TrunHistoryUI] Not Find Action");
+
+            _maxPage = (_act.Count / _ViewCount) + 1;
         }
         catch (Exception e)
         {
             Debug.Log(e);
         }
     }
-
-    public void OnChangeScroll()
+    
+    public void ChangeValue(int Value)
     {
+        _CurPage = Math.Clamp(_CurPage + Value, 1, _maxPage);
 
-    }
+        for(int i = 0; i < _ViewCount; i++)
+        {
+            int iIndex = (_CurPage * _ViewCount) + i;
 
-    private void OnEnable()
-    {
+            if (_act.Count <= iIndex)
+                _HistoryView[i].Close();
+            else
+            {
+                _HistoryView[i].Open();
+                //여기서 값 세팅
+            }
 
+        }
     }
 
     protected override void OnDestroy()
